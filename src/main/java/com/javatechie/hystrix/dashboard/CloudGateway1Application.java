7c94b56a9d2a1aec9f986877d8600d3048b509a3
pullatechie582 <pullarao.kanchupaty582@gmail.com>
2021-06-02 11:48:53 +0530

    first commit

---
+++ src/main/java/com/javatechie/hystrix/dashboard/CloudGateway1Application.java
@@ -0,0 +1,13 @@
+package com.javatechie.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGateway1Application.class, args);
+	}
+
+}
